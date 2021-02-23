49b723fd829218aa33a659c714eee586b440d889
hodayaYProject <hodiyoav@gmail.com>
2021-02-24 00:58:54 +0200

    call user element for fronted

--- fronted_testing.py
+++ fronted_testing.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path="C:\\Users\\Hodaya\\Downloads\\chromedriver.exe")
-user = driver.get("http://127.0.0.1:5001/user/get_user_data/179")
-print(user)
+driver.get("http://127.0.0.1:5001/user/get_user_data/179")
+user = driver.find_element_by_id("user")
+print(user.text)
