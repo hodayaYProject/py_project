2ff2b1052eb5afa5ccd93ade8e8e00865cff3935
hodayaYProject <hodiyoav@gmail.com>
2021-02-27 20:47:22 +0200

    try

--- web_app.py
+++ web_app.py
@@ -16,9 +16,9 @@ def stop_server():
 def get_user_name(user_id):
     user = Stock.get_user(user_id)
     if user:
-        return "<H1 id='user'>" "id: " + str(user.id) + " name: " + str(user.name) + "</H1>", 200
+        return "<h1 id='user'>" "id: " + str(user.id) + " name: " + str(user.name) + "</h1>", 200
     else:
-        return "<H1 id='error'> no such user: " + user_id + "</H1>", 500
+        return "<h1 id='error'> no such user: " + user_id + "</h1>", 500
 
 
 app.run(host='127.0.0.1', debug=True, port=5001)
