86e68e878d357a5f84aa63f482144c345ff2af47
hodayaYProject <hodiyoav@gmail.com>
2021-02-27 21:00:32 +0200

    test error

--- fronted_testing.py
+++ fronted_testing.py
@@ -3,6 +3,6 @@ driver = webdriver.Chrome(executable_path="C:\\Users\\Hodaya\\Downloads\\chromed
 driver.get("http://127.0.0.1:5001/user/get_user_data/179")
 driver.implicitly_wait(5)
 print('start')
-user = driver.find_element_by_xpath('//*[@id="user"]')
+# user = driver.find_element_by_xpath('//*[@id="user"]')
 # user = driver.find_element_by_id("user")
-print(user.text)
+# print(user.text)
